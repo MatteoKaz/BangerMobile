29e222e325abf7c8781ede68fe0aff696b43e491
MatteoKaz <matteokazan@gmail.com>
2026-04-08 20:42:01 +0200

    mk transi

--- Assets/Script/Manager/BilletManager.cs
+++ Assets/Script/Manager/BilletManager.cs
@@ -1,9 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class BilletManager : MonoBehaviour
 {
+    [SerializeField] private GameObject billet;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject HautEcran;
+    [SerializeField] private GameObject BasEcran;
     public void BilletAnim()
     {
+        billet.SetActive(true);
+        StartCoroutine(Anim());
+    }
+    public IEnumerator Anim()
+    {
+        yield return new WaitForSeconds(1f);
+        animator.SetTrigger("AnimBillet");
 
+        yield return new WaitForSeconds(12.5f);
+           billet.SetActive(false);
     }
 }
